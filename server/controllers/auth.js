62cb719a510536f73b2d70dc83d660e3c899424a
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2023-06-03 12:02:01 -0500

    test

---
+++ server/controllers/auth.js
@@ -0,0 +1,185 @@
+import User from "../models/user";
+import { hashPassword, comparePassword } from "../utils/auth";
+import jwt from "jsonwebtoken";
+import { nanoid } from "nanoid";
+import AWS from "aws-sdk";
+
+const awsConfig = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+  apiVersion: process.env.AWS_API_VERSION,
+};
+
+const SES = new AWS.SES(awsConfig);
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Validation
+    if (!name) {
+      return res.status(400).send("Name is required");
+    }
+    if (!password || password.length < 6) {
+      return res
+        .status(400)
+        .send("Password is required and should be at least 6 characters long");
+    }
+
+    const userExist = await User.findOne({ email }).exec();
+    if (userExist) {
+      return res.status(400).send("Email is taken");
+    }
+
+    // Hash password
+    const hashedPassword = await hashPassword(password);
+
+    // Register user
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+
+    return res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error. Try again.");
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user with the given email exists in the database
+    const user = await User.findOne({ email }).exec();
+    if (!user) {
+      return res.status(400).send("No user found");
+    }
+
+    // Check password
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.status(400).send("Wrong password");
+    }
+
+    // Create signed JWT
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    // Exclude hashed password from the user object
+    user.password = undefined;
+
+    // Send token in a cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      // secure: true, // only works on https
+    });
+
+    // Send user as JSON response
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error. Try again.");
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.json({ message: "Signout success" });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const currentUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password").exec();
+    console.log("CURRENT_USER", user);
+    return res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const shortCode = nanoid(6).toUpperCase();
+
+    const user = await User.findOneAndUpdate(
+      { email },
+      { passwordResetCode: shortCode }
+    );
+    if (!user) {
+      return res.status(400).send("User not found");
+    }
+
+    // Prepare email parameters
+    const params = {
+      Source: process.env.EMAIL_FROM,
+      Destination: {
+        ToAddresses: [email],
+      },
+      Message: {
+        Body: {
+          Html: {
+            Charset: "UTF-8",
+            Data: `
+              <html>
+                <h1>Reset password</h1>
+                <p>Use this code to reset your password</p>
+                <h2 style="color:red;">${shortCode}</h2>
+                <i>edemy.com</i>
+              </html>
+            `,
+          },
+        },
+        Subject: {
+          Charset: "UTF-8",
+          Data: "Reset Password",
+        },
+      },
+    };
+
+    const emailSent = SES.sendEmail(params).promise();
+    emailSent
+      .then((data) => {
+        console.log(data);
+        res.json({ ok: true });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  try {
+    const { email, code, newPassword } = req.body;
+    const hashedPassword = await hashPassword(newPassword);
+
+    const user = User.findOneAndUpdate(
+      {
+        email,
+        passwordResetCode: code,
+      },
+      {
+        password: hashedPassword,
+        passwordResetCode: "",
+      }
+    ).exec();
+
+    res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error! Try again.");
+  }
+};
---
+++ server/controllers/instructor.js
@@ -0,0 +1,69 @@
+import User from "../models/user";
+import Course from "../models/course";
+import queryString from "query-string";
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+export const makeInstructor = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).exec();
+    const updatedRoles = user.role.filter(role => role !== "Subscriber");
+    updatedRoles.push("Instructor");
+    user.role = updatedRoles;
+    const statusUpdated = await user.save();
+    res.json(statusUpdated);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+
+export const getAccountStatus = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).exec();
+    const account = await stripe.accounts.retrieve(user.stripe_account_id);
+    // console.log("ACCOUNT => ", account);
+    if (!account.charges_enabled) {
+      return res.status(401).send("Unauthorized");
+    } else {
+      const statusUpdated = await User.findByIdAndUpdate(
+        user._id,
+        {
+          stripe_seller: account,
+          $addToSet: { role: "Instructor" },
+        },
+        { new: true }
+      )
+        .select("-password")
+        .exec();
+      res.json(statusUpdated);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const currentInstructor = async (req, res) => {
+  try {
+    let user = await User.findById(req.user._id).select("-password").exec();
+    // console.log("CURRENT INSTRUCTOR => ", user);
+    if (!user.role.includes("Instructor")) {
+      return res.sendStatus(403);
+    } else {
+      res.json({ ok: true });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const instructorCourses = async (req, res) => {
+  try {
+    const courses = await Course.find({ instructor: req.user._id })
+      .sort({ createdAt: -1 })
+      .exec();
+    res.json(courses);
+  } catch (err) {
+    console.log(err);
+  }
+};
